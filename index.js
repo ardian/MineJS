ad7fb143ade36a7b750d2dccbf34fb703999997f
Ardian Haxha <ardian@brainpress.org>
2016-06-28 17:55:18 +0200

    Adding player and textures

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var createGame = require('voxel-engine');
 
-var game = createGame();
+var game = createGame({
+  texturePath: 'textures/'
+});
 var container = document.body;
 game.appendTo(container);
+
+var createPlayer = require('voxel-player')(game);
+
+var player = createPlayer('player.png');
+
+//# Puts the camera into the player
+player.possess();
+
+
+player.yaw.position.set(0, 100, 0);
